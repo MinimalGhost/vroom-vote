75ec5f9adc01a7b2812999404eaa282d5b5b2cbb
github username <github email address>
2018-02-27 10:38:30 -0500

    init commit

---
+++ vroom-vote-api/app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::AuthController < ApplicationController
+  def show
+    render json: current_user
+  end
+
+  def create
+    # check for user with this email
+    user = User.find_by(email: params[:email])
+    # if it exists, make sure password matches
+    if user.present? && user.authenticate(params[:password])
+      # if password matches, render back a json response of user info
+      created_jwt = issue_token({id: user.id})
+      render json: {user: user, jwt: created_jwt}
+    else
+      # otherwise, render back an error response
+      render json: {
+        error: 'Email or password incorrect'
+      }, status: 404
+    end
+  end
+
+  def signup
+    user = User.new(user_params)
+    if user.save
+      created_jwt = issue_token({id: user.id})
+      render json: {user: user, jwt: created_jwt}
+    else
+      render json: {errors: user.errors.full_messages}, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
